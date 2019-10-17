118f2d5ef9a06e5748a8a9aff7e7ed55f82e4ed3
Pedro Fontes <pvfontes1000@gmailcom>
2019-10-17 08:45:58 -0300

    seletor atualiza dados

--- src/categories/Ship.js
+++ src/categories/Ship.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import style from './ship.module.css'
+import style from './item.module.css'
 
-const Ship = ({name, title, model, cost, clas})=>{
+const Ship = ({name, model, cost, clas})=>{
     return(
-        <div className={style.Ship}>
-            <h1>{title}</h1>
+        <div className={style.Item}>
             <h1>{name}</h1>
             <b>Class:</b>
             <p>{clas}</p>
